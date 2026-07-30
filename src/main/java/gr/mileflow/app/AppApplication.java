cc658603fca5570dcea0d17ceb35e5dcb6e13728
akounalake <kounalaki.artemis@yahoo.com>
2026-07-30 18:20:16 +0300

    build: Initial project setup

---
+++ src/main/java/gr/mileflow/app/AppApplication.java
@@ -0,0 +1,13 @@
+package gr.mileflow.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gr/mileflow/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.mileflow.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
