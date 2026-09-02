82f4a940e5dba1de7a5358708443735b7fd86001
akounalake <kounalaki.artemis@yahoo.com>
2026-09-02 13:00:22 +0300

    chore: add v2 migration - sports

---
+++ src/main/resources/db/migration/V2__insert_sports.sql
@@ -0,0 +1,14 @@
+INSERT INTO sports (name, created_at, updated_at, deleted)
+VALUES
+('100m', NOW(), NOW(), 0),
+('200m', NOW(), NOW(), 0),
+('400m', NOW(), NOW(), 0),
+('800m', NOW(), NOW(), 0),
+('1500m', NOW(), NOW(), 0),
+('3000m', NOW(), NOW(), 0),
+('5000m', NOW(), NOW(), 0),
+('10000m', NOW(), NOW(), 0),
+('110m Hurdles', NOW(), NOW(), 0),
+('400m Hurdles', NOW(), NOW(), 0),
+('Long Jump', NOW(), NOW(), 0),
+('High Jump', NOW(), NOW(), 0);
